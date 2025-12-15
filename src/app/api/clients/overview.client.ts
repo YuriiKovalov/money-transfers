12c2cf2eb9e2afec31c0daacc45df37b8e78fe02
YuriiKovalov <kovalovy76@gmail.com>
2025-12-15 15:31:30 +0200

    feat: mock and client

---
+++ src/app/api/clients/overview.client.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { delay, of } from 'rxjs';
+import {
+  AccountBalanceResponse,
+  ConnectedAccountsResponse,
+  RecentTransfersResponse,
+} from '../models/overview.models';
+import accountBalanceMock from '../mocks/account-balance.mock.json';
+import connectedAccountsMock from '../mocks/connected-accounts.mock.json';
+import recentTransfersMock from '../mocks/recent-transfers.mock.json';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class OverviewClient {
+  getAccountBalance() {
+    const response = accountBalanceMock as AccountBalanceResponse;
+    return of(response).pipe(delay(300));
+  }
+
+  getConnectedAccounts() {
+    const response = connectedAccountsMock as ConnectedAccountsResponse;
+    return of(response).pipe(delay(300));
+  }
+
+  getRecentTransfers() {
+    const response = recentTransfersMock as RecentTransfersResponse;
+    return of(response).pipe(delay(300));
+  }
+}
